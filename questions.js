978ba635b1f6867966ac9bdfc40b242adbe68ccc
Jose Leuriberto Maia Neto <leuriberto.neto@gmail.com>
2025-06-28 09:14:34 -0300

    Meu primeiro Commit

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    { // 01
+        question: "01. O que significa CRUD:",
+        answers: [
+            { option: "Creat, Read, Update e Delete.", correct: true},
+            { option: "Care, Reload, Update e Delete.", correct: false},
+            { option: "Creat, Read, Using e Date.", correct: false},
+        ],
+    },
+    {
+        question: "02. Como exibir uma informação no Output em JavaScript:",
+        answers: [
+            { option: "print()", correct: false},
+            { option: "console.log()", correct: true},
+            { option: "exibir()", correct: false},
+        ],
+    },
+    {
+        question: "03. Qual é a função de uma linguagem Back-end:",
+        answers: [
+            { option: "Lógica de processamento, mantendo a comunicação com banco de dados e interação com o servidor.", correct: true},
+            { option: "Definir o layout das páginas, trazendo riqueza aos detalhes.", correct: false},
+            { option: "Criar e implementar interfaces gráficas. ", correct: false},
+        ],
+    },
+    {
+        question: "03. Quem foi o criador do SO Linux:",
+        answers: [
+            { option: "Steve Jobs.", correct: false},
+            { option: "Jeff Bezos.", correct: false},
+            { option: "Linus Torvalds. ", correct: true},
+        ],
+    },
+];
